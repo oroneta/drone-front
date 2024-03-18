889c8afb38cfbbabae4127e8fcd76ecc3b9603e4
achechi15 <142111179+achechi15@users.noreply.github.com>
2024-03-18 15:44:23 +0100

    1st version Drones Section

--- src/src/layouts/drones/components/DroneInfo/index.js
+++ src/src/layouts/drones/components/DroneInfo/index.js
@@ -10,29 +10,46 @@ import DroneImage from '../image/droneImage.png';
 import VuiBadge from "components/VuiBadge";
 
 // React Icons
-import { RiBattery2ChargeFill } from "react-icons/ri";
+import { FaBatteryFull, FaBatteryThreeQuarters, FaBatteryHalf, FaBatteryQuarter, FaBatteryEmpty } from "react-icons/fa6";
+import { MdWifi, MdWifi2Bar, MdWifi1Bar, MdSignalWifiStatusbarNull } from "react-icons/md"; // MdWifi = lleno, el númro después de MdWifi representa el número de barras
+import { BiSignal5, BiSignal4, BiSignal3, BiSignal2, BiSignal1, BiNoSignal } from "react-icons/bi";
+
 
 const DroneInfo = ({ height }) => {
 	const { gradients } = colors;
 	const { cardContent } = gradients;
 
 	return (
-		<Card sx={{ height: {height} }}>
+		<Card sx={{ height: {height}, display:"flex"}}>
 			<VuiBox display='flex' flexDirection='column'>
 				<VuiTypography variant='lg' color='white' fontWeight='bold' mb='4px'>
 					Drone Information
 				</VuiTypography>
-				<VuiBox sx={{ alignSelf: 'center', justifySelf: 'center', zIndex: '-1' }} mt={5}>
+				<VuiBox sx={{ display: "flex", justifyContent: "space-between"}}>
+					<VuiBadge
+						variant="standard"
+						badgeContent="Online"
+						color="success"
+						size="xs"
+						container
+						sx={({ palette: { white, success }, borders: { borderRadius, borderWidth } }) => ({
+							background: success.main,
+							border: `${borderWidth[1]} solid ${success.main}`,
+							borderRadius: borderRadius.md,
+							color: white.main,
+						})}
+					/>
+					<VuiBox sx={{padding: "0px 10px"}}>
+						<BiSignal5 color='white' style={{marginRight: "10px"}} />
+						<MdWifi color='white' style={{marginRight: "10px"}} />
+						<FaBatteryThreeQuarters color='white' style={{marginRight: "10px"}} />
+					</VuiBox>
+					
+				</VuiBox>
+				<VuiBox sx={{ alignSelf: 'center', justifySelf: 'center', zIndex: '-1' }}>
 					<VuiBox sx={{ position: 'relative', display: 'inline-flex' }}>
-					{/* <VuiBadge
-					variant="standard"
-					badgeContent="Online"
-					color="success"
-					size="xs"
-					item
-					sx={100}
-					/> */}
-						{/* <RiBattery2ChargeFill color='white' /> */}
+					
+						
 						<CircularProgress variant='determinate' value={87} size={170} color='info' />
 						<VuiBox
 							sx={{
